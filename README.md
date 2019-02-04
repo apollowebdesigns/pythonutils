ee889757d43b320f3df619cd93894f32d56fc667
apollowebdesigns <andrewevans0@outlook.com>
2019-02-04 19:16:59 +0000

    adding in harddrive

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pythonutils
+
+## Useful links
+
+[fstab file to stort out](https://apple.stackexchange.com/questions/174879/how-to-write-fstab-rules-for-an-ntfs-volume-if-the-volume-has-no-label-or-uuid)
--- harddrive.py
+++ harddrive.py
@@ -1,3 +1,12 @@
 import os
 
-os.system('echo "hello world"')
+def info_of_hard_drive(hard_drive_name):
+    os.system('diskutil info /Volumes/' + hard_drive_name)
+
+
+info_of_hard_drive('My\ Passport')
+
+
+def nfts_convert():
+    # Key in your uuid for your hard drive from previous command
+    os.system('UUID=5B5C4986-DB46-4AB7-9244-FA3D1B2D1DCE none ntfs rw,auto,nobrowse')
